71fce68ec928eb70142cfbf647a7e16ad730f24c
priyajain9923 <134014477+priyajain9923@users.noreply.github.com>
2024-08-11 22:01:35 -0700

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,17 @@ import io
 import openai
 import nltk
 from nltk.corpus import stopwords
+
+# Access the environment variable
+#OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+st.write(f"OPENAI_API_KEY: {OPENAI_API_KEY}")
+
+if OPENAI_API_KEY is None:
+    st.error("The OPENAI_API_KEY environment variable is not set.")
+else:
+    st.success("OPENAI_API_KEY is set.")
+    openai.api_key = OPENAI_API_KEY
+    client = openai  # Use this line instead of openai.OpenAI(api_key=...)
    
 # Instantiate the OpenAI client
 #client = openai.OpenAI(api_key=OPENAI_API_KEY)
